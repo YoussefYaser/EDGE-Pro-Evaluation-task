e421afa69cd87d5f591fc8879e3fbb299e5882bb
YoussefYaser <youssefyasser549@gmail.com>
2024-10-20 19:20:20 +0300

    add custom marker to map section

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,9 +2,17 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import './Map.css'
 import 'leaflet/dist/leaflet.css'
 import { forwardRef } from 'react'
+import  L  from 'leaflet'
+import marker from '../../assets/placeholder.png'
 
 
 const Map = forwardRef(function Map(props, {sections}) {
+
+    
+    const customMarker = L.icon({
+        iconUrl : marker,
+        iconSize : [38, 38]
+    })
 
     return (
         <section ref={home=>sections.current[0] = home} id='home' className=" map ">
@@ -15,7 +23,7 @@ const Map = forwardRef(function Map(props, {sections}) {
                             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
-                        <Marker position={[30.029883693373808, 31.459625089094196]}>
+                        <Marker position={[30.029883693373808, 31.459625089094196]} icon={customMarker}>
                             <Popup>
                                 Egypt, Cairo <br /> Edge pro for information systems
                             </Popup>
